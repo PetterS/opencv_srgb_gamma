Fix incorrect value of linear breakpoint. This fixes #2.
Tests were not good enough but have been improved.

srgb.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def to_linear(srgb):
 	linear = np.float32(srgb) / 255.0
-	less = linear <= 0.4045
+	less = linear <= 0.04045
 	linear[less] = linear[less] / 12.92
 	linear[~less] = np.power((linear[~less] + 0.055) / 1.055, 2.4)
 	return linear
@@ -21,4 +21,6 @@ if __name__ == "__main__":
 	# Test
 	for x in [0., 0.000123, 0.345, 0.9, 1.0]:
 		assert abs(x - to_linear(from_linear(np.array([x])))) < 1e-6
+	for x in range(256):
+		assert round(x - from_linear(to_linear(np.array([x])))[0]) == 0
 	print("Test OK.")